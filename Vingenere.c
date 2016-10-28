3faa10579e7281c290e195c024c23d012826df45
mykolamatsiakh <mmatsiakh@gmail.com>
2016-10-28 22:16:52 +0300

    Clean code:step2

--- Vingenere.c
+++ Vingenere.c
@@ -3,21 +3,20 @@
 #include <ctype.h>
 #include <string.h>
 
-
-int main(int argc, string argv[])
+int main(int argc, string argv[]) // Оголошуєм у функції аргумент argc та масив стрічки argv[] 
 {
-  if (argc != 2)
-  {
-   printf("Try again.");
-   return 1;
-  }
-    
-  string key = argv[1];
-  int keyCount = strlen(key);
   
+	if (argc != 2)       //Перевіряємо чи аргумент argc типу integer не дорівнює 2
+	  {
+   		printf("Try again.");
+   		return 1;
+  	  }
+    
+  string key = argv[1];         //Оголошуємо стрічку key ,якій присвоюється елемент масиву argv
+  int keyCount = strlen(key);   //Оголошуємо змінну keyCount і присвоюємо їй значення рівне довжині стрічки key
   for(int k = 0; k < keyCount; k++) 
   {
-   if (!isalpha(key[k]))
+   if (!isalpha(key[k]))        //Перевіряємо чи к-елемент масиву key є літерою
     {
      printf("Try again.");
      return 1;
@@ -26,24 +25,24 @@ int main(int argc, string argv[])
     
    string text = GetString();
    if(text == NULL) 
-   {
+     {
     printf("Try again.");
     return 1;
-   }
+     }
     
     for (int i = 0, n = 0; i < strlen(text); i++) 
    {
-    if (isalpha(text[i])) 
+      if (isalpha(text[i])) //Якщо елементи масиву є літерами
     {
-    if (isupper(text[i]))
-    printf("%c", ((((text[i] - 'A') + ((toupper(key[n++%keyCount]))-'A')%26) % 26) + 'A'));
+      if (isupper(text[i]))                                                                   
+          printf("%c", ((((text[i] - 'A') + ((toupper(key[n++%keyCount]))-'A')%26) % 26) + 'A'));  // Робимо маленькі літери великими
             
-    if (islower(text[i]))
-    printf("%c", ((((text[i] - 'a') + ((tolower(key[n++%keyCount]))-'a')%26) % 26) + 'a'));
+      if (islower(text[i]))
+         printf("%c", ((((text[i] - 'a') + ((tolower(key[n++%keyCount]))-'a')%26) % 26) + 'a'));
     } else
-     printf("%c", text[i]);
+         printf("%c", text[i]);
    }
     
-  printf("\n");
-  return 0;
+         printf("\n");
+         return 0;
 }
